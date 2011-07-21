48594a0ad60c2daf31888c807f94a89204d4bbfb
Margy Hockett <margy.hockett@sungardhe.com>
2011-07-21 10:16:00 -0400

    fix ddl

--- src/ddl/banner_upgrade.sql
+++ src/ddl/banner_upgrade.sql
@@ -6,6 +6,15 @@ set scan on echo on termout on;
 
 REM spool horizon_upgrade.lis
 
+connect bansecr/&&bansecr_password
+
+ insert into gurucls ( gurucls_userid, gurucls_class_code, gurucls_activity_date,
+                      gurucls_user_id)
+  select  'GRAILS_USER', 'BAN_PAYROLL_C', sysdate, user
+    from dual
+   where not exists (select 1 from gurucls
+                      where gurucls_userid = 'GRAILS_USER'
+                        and gurucls_class_code = 'BAN_PAYROLL_C');
 
 connect dbeu_owner/&&dbeu_password
 
