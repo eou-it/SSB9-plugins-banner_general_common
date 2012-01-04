68a61194a0a68a5900eab89c760acc6304da18de
praveenv <praveen.vysyaraju@sungard.com>
2012-01-04 16:11:41 +0530

    update the banner-general-common version to 1.0.4 with grails 1.3.7

--- BannerGeneralCommonGrailsPlugin.groovy
+++ BannerGeneralCommonGrailsPlugin.groovy
@@ -40,11 +40,11 @@ class BannerGeneralCommonGrailsPlugin {
     // more control on 'when' a grails app is updated to use a newer plugin version, and therefore 'could' allow delayed testing within those apps
     // independent of deploying a new plugin build to Nexus.
     //
-    String version = "1.0.3"
+    String version = "1.0.4"
 //    String version = "0.1.0"
 
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "1.3.5 > *"
+    def grailsVersion = "1.3.7 > *"
 
     // the other plugins this plugin depends on
 //    def dependsOn = ['springSecurityCore': '1.2']
