ef3863d90c9d3c0db456967e18a4c54024a3e75a
Margy Hockett <margy.hockett@sungardhe.com>
2013-06-07 14:26:09 -0400

    HFC 2264 add common utility for gtvsdax in application session

---
+++ grails-app/utils/net/hedtech/banner/general/GeneralCommonUtility.groovy
@@ -0,0 +1,38 @@
+/** *****************************************************************************
+ Copyright 2009-2012 Ellucian Company L.P. and its affiliates.
+ ****************************************************************************** */
+
+package net.hedtech.banner.general
+
+import org.codehaus.groovy.grails.web.context.ServletContextHolder as SCH
+
+import net.hedtech.banner.general.system.SdaCrosswalkConversion
+
+/**
+ * This is a utilty class for all of Banner
+ *
+ */
+class GeneralCommonUtility {
+
+    /**
+     * Store gtvsdax values in  application variable to minimize trips to the DB
+     * @param internal
+     * @param internalGroup
+     * @return
+     */
+
+    public static gtvsdaxForSession(def internal, def internalGroup) {
+        def gtvsdaxList = SCH.servletContext.getAttribute("gtvsdax")
+        if (!gtvsdaxList) gtvsdaxList = [:]
+        def gtvsdaxValue = gtvsdaxList?.find { it.key == internal }?.value
+
+        if (!gtvsdaxValue) {
+            gtvsdaxValue = SdaCrosswalkConversion.fetchAllByInternalAndInternalGroup(internal, internalGroup)[0]?.external[0]
+            gtvsdaxList.put(internal, gtvsdaxValue)
+            SCH.servletContext.setAttribute("gtvsdax", gtvsdaxList)
+        }
+        return gtvsdaxValue
+    }
+
+
+}
---
+++ test/integration/net/hedtech/banner/general/GeneralCommonUtilityIntegrationTests.groovy
@@ -0,0 +1,36 @@
+/** *****************************************************************************
+ Copyright 2009-2012 Ellucian Company L.P. and its affiliates.
+ ****************************************************************************** */
+
+package net.hedtech.banner.general
+
+import net.hedtech.banner.general.system.SdaCrosswalkConversion
+import net.hedtech.banner.testing.BaseIntegrationTestCase
+import org.codehaus.groovy.grails.web.context.ServletContextHolder as SCH
+
+class GeneralCommonUtilityIntegrationTests extends BaseIntegrationTestCase {
+
+
+    protected void setUp() {
+        formContext = ['GUAGMNU']
+        super.setUp()
+    }
+
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+
+    void testGtvsdaxSet() {
+        SCH.servletContext.removeAttribute("gtvsdax")
+
+        def gtvsdaxValue = SdaCrosswalkConversion.fetchAllByInternalAndInternalGroup('SCHBYDATE', 'WEBREG')[0]?.external[0]
+        def schByDate = GeneralCommonUtility.gtvsdaxForSession('SCHBYDATE', 'WEBREG')
+        assertEquals schByDate, gtvsdaxValue
+        def sdaxList = SCH.servletContext.getAttribute("gtvsdax")
+        assertNotNull sdaxList
+        assertTrue sdaxList.containsKey("SCHBYDATE")
+
+    }
+}
