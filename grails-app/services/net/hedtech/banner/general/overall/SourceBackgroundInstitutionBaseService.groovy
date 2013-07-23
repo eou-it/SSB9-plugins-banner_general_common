88455034a79d5c3b793ff21668e7d6b149b2e353
Phong Nguyen <phong.nguyen@ellucian.com>
2013-07-23 09:34:20 -0400

    Removed getEnrollmentPlanningServiceCode from SourceBackgroundInstitutionBaseService

--- grails-app/services/net/hedtech/banner/general/overall/SourceBackgroundInstitutionBaseService.groovy
+++ grails-app/services/net/hedtech/banner/general/overall/SourceBackgroundInstitutionBaseService.groovy
@@ -7,9 +7,6 @@ import net.hedtech.banner.exceptions.ApplicationException
 import net.hedtech.banner.general.system.SourceAndBackgroundInstitution
 import net.hedtech.banner.service.ServiceBase
 
-import java.sql.CallableStatement
-import java.sql.SQLException
-
 class SourceBackgroundInstitutionBaseService extends ServiceBase {
 
     def preCreate(map) {
@@ -21,36 +18,6 @@ class SourceBackgroundInstitutionBaseService extends ServiceBase {
     def preUpdate(map) {
         validateCodes(map.domainModel)
         checkAddress(map.domainModel)
-    }
-
-
-    def getEnrollmentPlanningServiceCode(sourceAndBackgroundInstitution) {
-        def sbgi = SourceBackgroundInstitutionBase.fetchBySourceAndBackgroundInstitution(sourceAndBackgroundInstitution)
-        def epsCode
-
-        if (sbgi) {
-            try {
-                CallableStatement sqlCall = sessionFactory.getCurrentSession().connection().prepareCall("{? = call f_EpscCode(?, ?, ?)}")
-                sqlCall.registerOutParameter(1, java.sql.Types.VARCHAR)
-                sqlCall.setString(2, sbgi?.state?.code)
-                sqlCall.setString(3, sbgi?.zip)
-                sqlCall.setString(4, sbgi?.county?.code)
-                sqlCall.executeUpdate()
-                epsCode = sqlCall.getString(1)
-            }
-            catch (SQLException ae) {
-                log.debug "SqlException in getEnrollmentPlanningServiceCode exception ${ae}"
-                log.debug ae.stackTrace
-                throw ae
-            }
-            catch (Exception ae) {
-                log.debug "Exception in getEnrollmentPlanningServiceCode ${ae} "
-                log.debug ae.stackTrace
-                throw ae
-            }
-        }
-
-        return epsCode
     }
 
 
