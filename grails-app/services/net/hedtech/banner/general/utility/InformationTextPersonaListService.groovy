a1b3f8740254bfe40eaa8ed73712944bc2ce59c9
shrridevis <shrridevi.sreedharan@ellucian.com>
2013-08-22 15:01:07 +0530

    HRU-4629- Removing webTailorRoleInformation.groovy as the contents of this file will be added in banner_configuration.groovy and modifying service to handle this change

--- grails-app/services/net/hedtech/banner/general/utility/InformationTextPersonaListService.groovy
+++ grails-app/services/net/hedtech/banner/general/utility/InformationTextPersonaListService.groovy
@@ -8,61 +8,64 @@ import org.apache.commons.lang.StringUtils
 
 import java.text.ParseException
 import java.text.SimpleDateFormat
+import org.codehaus.groovy.grails.commons.ConfigurationHolder as CH
 
 class InformationTextPersonaListService {
     def sessionFactory
 
-    def static webTailorRoleList
+    def static webTailorRoleList = []
 
 
     static {
-        webTailorRoleList = new ConfigSlurper().parse(InformationTextPersonaListService.classLoader.loadClass("WebTailorRoleInformation"))
+        if (CH?.config.webTailorRoleList.size() != 0) {
+            webTailorRoleList = CH?.config.webTailorRoleList
+        }
     }
 
-    public List fetchInformationTextPersonas( String filter ) {
+    public List fetchInformationTextPersonas(String filter) {
         def sql
-        def result=[]
+        def result = []
         def existsSql = """SELECT 'Y' as tableExists FROM ALL_TABLES WHERE TABLE_NAME = 'GURINFO'"""
-        try{
-            sql = new Sql( sessionFactory.currentSession.connection() )
+        try {
+            sql = new Sql(sessionFactory.currentSession.connection())
             result = sql.firstRow(existsSql)
-        }finally {
+        } finally {
             sql?.close()
         }
-        if(!(result?.tableExists as boolean)) {
+        if (!(result?.tableExists as boolean)) {
             return []
         }
 
-        if (StringUtils.isBlank( filter )) {
+        if (StringUtils.isBlank(filter)) {
             filter = "%"
         } else if (!(filter =~ /%/)) {
             filter += "%"
         }
 
-        List filteredWebTailorRoleList = filterAndOrderWebTailorRoleList (webTailorRoleList, filter)
+        List filteredWebTailorRoleList = filterAndOrderWebTailorRoleList(webTailorRoleList, filter)
 
         List informationTextPersonaList = []
         filteredWebTailorRoleList?.each { webTailorRole ->
             informationTextPersonaList << new InformationTextPersona(
-                                                [
-                                                        code: webTailorRole.code,
-                                                        description: webTailorRole.description,
-                                                        lastModifiedBy: webTailorRole.lastModifiedBy,
-                                                        lastModified: getDate (webTailorRole.lastModified, "dd-MMM-yy")
-                                                ] )
+                    [
+                            code: webTailorRole.code,
+                            description: webTailorRole.description,
+                            lastModifiedBy: webTailorRole.lastModifiedBy,
+                            lastModified: getDate(webTailorRole.lastModified, "dd-MMM-yy")
+                    ])
 
         }
 
         return informationTextPersonaList
     }
 
-    private List filterAndOrderWebTailorRoleList(Map webTailorRoleList, String filter) {
+    private List filterAndOrderWebTailorRoleList(List webTailorRoleList, String filter) {
         String filterColumn = "code"
 
         String regEx = this.getRegEx(filter)
 
         List filteredResults
-        filteredResults = webTailorRoleList.values().toList().get(0).findAll {
+        filteredResults = webTailorRoleList.findAll {
             it."$filterColumn".toString().toLowerCase().matches(regEx)
         }
 
@@ -92,14 +95,14 @@ class InformationTextPersonaListService {
         return regEx
     }
 
-    private Date getDate (String dateString, String format) {
+    private Date getDate(String dateString, String format) {
         SimpleDateFormat sdf = new SimpleDateFormat(format)
         sdf.setLenient(false)
         try {
             return sdf.parse(dateString)
         } catch (ParseException e) {
             // Ignore because its not the right format.
-            log.warn (" Date was not in the $format format" )
+            log.warn(" Date was not in the $format format")
         }
         return null
     }
