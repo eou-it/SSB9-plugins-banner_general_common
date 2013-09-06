0904392fc66de6db80a3bb50eb0d18a038254660
Navein Austin Fernandes <Navein.Fernandes@ellucian.com>
2013-09-06 14:17:24 +0530

    HRU-4661 & HRU-4663. Moving of JS, CSS and images for Terms of Usage and Security Questions to banner_general_common from banner_core and banner_general_person

---
+++ grails-app/conf/BannerGeneralCommonSelfServiceResources.groovy
@@ -0,0 +1,24 @@
+
+/** *****************************************************************************
+ Copyright 2009-2012 Ellucian Company L.P. and its affiliates.
+ ****************************************************************************** */
+
+modules = {
+
+    'userAgreement' {
+        dependsOn "bannerSelfService, i18n-core"
+        defaultBundle environment == "development" ? false : "userAgreement"
+        //defaultBundle false
+
+        resource url: [plugin: 'banner-general-common', file: 'css/views/userAgreement/policy.css'], attrs: [media: 'screen, projection']
+        resource url: [plugin: 'banner-general-common', file: 'js/views/userAgreement/policy.js']
+    }
+
+    'securityQA' {
+        dependsOn "bannerSelfService, i18n-core"
+        defaultBundle environment == "development" ? false : "securityQA"
+        resource url: [plugin: 'banner-general-common', file: 'css/views/securityQA/securityQA.css'], attrs: [media: 'screen, projection']
+        resource url: [plugin: 'banner-general-common', file: 'js/views/securityQA/securityQA.js']
+    }
+
+}
---
+++ web-app/js/views/userAgreement/policy.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $("#policy-continue").click(function () {
+        var href = $(this).attr("data-endpoint");
+        window.location = href;
+    });
+    $("#policy-exit").click(function () {
+        var href = $(this).attr("data-endpoint");
+        window.location = href;
+    });
+
+})
