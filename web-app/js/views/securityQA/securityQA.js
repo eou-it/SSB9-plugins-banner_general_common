f3f2c774f8e30ff693996f388ef28cc8243dc08d
SphoortiA <Sphoorti.Acharya@sungardhe.com>
2013-09-17 15:18:21 +0530

    HRU-4671 accessibility support for securityQA

--- web-app/js/views/securityQA/securityQA.js
+++ web-app/js/views/securityQA/securityQA.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+    $('#pin').focus();
+    var ariaValid = false;
 
     EventDispatcher.addEventListener(Application.events.initialized, function () {
         if (window.securityQAInitErrors && window.securityQAInitErrors.notification && window.securityQAInitErrors.notification.length > 0) {
@@ -33,7 +35,9 @@ $(document).ready(function () {
         notifications.clearNotifications()
         validatePin();
 
+
         if(userDefinedQuesFlag == 'Y') {
+            $('body').append('<div id="aria-invalid-error"></div>');
 
             $('input#userDefinedQuestion').each(function (j, selectElm) {
                 var enteredText = $(selectElm).val();
@@ -41,10 +45,15 @@ $(document).ready(function () {
                 var invalidqusetionlength = $.i18n.prop("securityQA.invalid.length.question", [questionMinimumLength]);
                 if ((enteredText.length > 0) && (enteredText.match('<') || enteredText.match('>'))) {
                     $(selectElm).parent().addClass("notification-error");
+
+                    addAriaErrors(selectElm, invalidcharacter);
+
                     notificationMessages.push(invalidcharacter);
                 }
                 if (enteredText.length > 0 && enteredText.length < questionMinimumLength) {
                     $(selectElm).parent().addClass("notification-error");
+
+                    addAriaErrors(selectElm, invalidqusetionlength);
                     notificationMessages.push(invalidqusetionlength);
                 }
             });
@@ -58,6 +67,7 @@ $(document).ready(function () {
                 if (index == 0) {
                     var error = $.i18n.prop("securityQA.error");
                     $(ielm).closest("div .section-wrapper").addClass("notification-error");
+                    addAriaErrors(ielm, error);
                     notificationMessages.push(error);
                 }
             } else {
@@ -65,11 +75,13 @@ $(document).ready(function () {
                 if (index != 0 && userDefinedQuestion.length > 0) {
                     var error = $.i18n.prop("securityQA.invalid.number.question");
                     $(ielm).closest("div .section-wrapper").addClass("notification-error");
+                    addAriaErrors(ielm, error);
                     notificationMessages.push(error);
                 }
                 else if (index == 0 && userDefinedQuestion.length == 0) {
                     var error = $.i18n.prop("securityQA.error");
                     $(ielm).closest("div .section-wrapper").addClass("notification-error");
+                    addAriaErrors(ielm, error);
                     notificationMessages.push(error);
                 }
             }
@@ -83,21 +95,44 @@ $(document).ready(function () {
                 var error = $.i18n.prop("securityQA.error");
                 notificationMessages.push(error);
                 $(ielm).parent().addClass("notification-error");
+                addAriaErrors(ielm, error);
             }
 
             if ((enteredText.length > 0) && (enteredText.match('<') || enteredText.match('>'))) {
                 var invalidcharacter = $.i18n.prop("securityQA.invalid.answer");
                 $(ielm).parent().addClass("notification-error");
+                addAriaErrors(ielm, invalidcharacter);
                 notificationMessages.push(invalidcharacter);
             }
             if (enteredText.length > 0 && enteredText.length < answerMinimumLength) {
                 var invalidanswerlength = $.i18n.prop("securityQA.invalid.length.answer", [answerMinimumLength]);
                 $(ielm).parent().addClass("notification-error");
+                addAriaErrors(ielm, invalidanswerlength);
                 notificationMessages.push(invalidanswerlength);
             }
         });
     }
 
+    function addAriaErrors(ielm, error) {
+
+        $(ielm).attr('aria-invalid', 'true');
+        if(!ariaValid) {
+            ariaValid = true;
+            $(ielm).attr('aria-describedby', 'aria-invalid-error');
+        }
+
+        var found = false;
+        _.each( notificationMessages, function(msg) {
+            if(error === msg) {
+                found = true;
+            }
+        } , this );
+        if(found) {
+            return;
+        }
+
+        $('#aria-invalid-error').append('<p style="position: absolute;left: -9999px;">' + error + '</p>');
+    }
 
     $("#security-cancel-btn").click(function () {
         var href = $(this).attr("data-endpoint")
