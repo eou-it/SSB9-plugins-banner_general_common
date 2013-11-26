17bcbdf2dd0c68764fb83f5ae06609bfe522268f
shrridevis <shrridevi.sreedharan@ellucian.com>
2013-11-27 00:16:18 +0530

    HRU-4840 Incorporating code review comments on page to page navigation

--- src/groovy/net/hedtech/banner/web/SsbURLRequest.groovy
+++ src/groovy/net/hedtech/banner/web/SsbURLRequest.groovy
@@ -7,10 +7,11 @@ package net.hedtech.banner.web
 class SsbURLRequest {
     private static final String SSB_BASE_URL = "ssb"
     private static final String SLASH = "/"
+    private static final int RELATIVE_SLASH_INDEX = 1
 
     public String getControllerNameFromPath(String url) {
         if (url != null && url.contains(SSB_BASE_URL)) {
-            url = url.substring(url.indexOf(SSB_BASE_URL) + SSB_BASE_URL.length() + 1);
+            url = url.substring(url.indexOf(SSB_BASE_URL) + SSB_BASE_URL.length() + RELATIVE_SLASH_INDEX);
             if (url.contains(SLASH)) {
                 url = url.substring(0, url.indexOf(SLASH))
             }
