65c74bc0e4bf91c8fd403a4182d037d1c164365b
Margy Hockett <margy.hockett@ellucian.com>
2014-03-20 11:51:56 -0400

    Remove banner db main plugin

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -10,7 +10,6 @@ grails.plugin.location.'banner-core' = "../banner_core.git"
 grails.plugin.location.'banner-codenarc' = "../banner_codenarc.git"
 grails.plugin.location.'banner-general-validation-common' = "../banner_general_validation_common.git"
 grails.plugin.location.'banner-general-person' = "../banner_general_person.git"
-grails.plugin.location.'banner-db-main' = "../banner_db_main.git"
 grails.plugin.location.'banner-seeddata-catalog' = "../banner_seeddata_catalog.git"
 grails.plugin.location.'i18n-core' = "../i18n_core.git"
 
