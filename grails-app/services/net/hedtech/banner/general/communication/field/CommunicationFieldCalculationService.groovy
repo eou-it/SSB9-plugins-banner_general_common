e6fc39badd69f2e141236171ac2d2c92a923ec42
Ed Delaney <edelaney@ellucian.com>
2014-11-12 20:40:57 -0500

    Add support for datafield arrayoutput

--- grails-app/services/net/hedtech/banner/general/communication/field/CommunicationFieldCalculationService.groovy
+++ grails-app/services/net/hedtech/banner/general/communication/field/CommunicationFieldCalculationService.groovy
@@ -12,6 +12,8 @@ package net.hedtech.banner.general.communication.field
 
 import groovy.sql.GroovyRowResult
 import groovy.sql.Sql
+import net.hedtech.banner.exceptions.ApplicationException
+import net.hedtech.banner.general.person.PersonUtility
 import net.hedtech.banner.service.ServiceBase
 import org.stringtemplate.v4.ST
 
@@ -20,17 +22,38 @@ import java.util.regex.Pattern
 
 class CommunicationFieldCalculationService extends ServiceBase {
 
+    String calculateFieldByBannerId( String immutableId, String bannerId ) {
+
+        def person = PersonUtility.getPerson( bannerId )
+
+        if (person == null) {
+            throw new ApplicationException( CommunicationFieldCalculationService, "Person is null" ) // TODO: I18N these
+        }
+        if (immutableId == null) {
+            throw new ApplicationException( CommunicationFieldCalculationService, "ImmutableId is null" )
+        }
+
+        calculateFieldByPidm( immutableId, person.pidm )
+    }
+
+
+    String calculateFieldByPidm( String immutableId, Long pidm ) {
+        def sqlParams = [:]
+        sqlParams << ['pidm': pidm]
+        calculateField( immutableId, sqlParams )
+    }
+
     /**
      * Executes a data function and returns result set
      * @param communicationFieldId The id of the communication field
      * @param parameters Map of parameter values
      * @return
      */
-    String calculateField( String immutableId, Map parameters ) {
-
+    private String calculateField( String immutableId, Map parameters ) {
         def Sql sql
-
         CommunicationField communicationField = CommunicationField.findByImmutableId( immutableId )
+        // ToDo: decide if the upper bound should be configurable
+        int maxRows = (!communicationField.returnsArrayArguments) ? 1 : 50
         String statement = communicationField.ruleContent
         if (statement == null) {
             /* there is no statement to execute, so just return the formatter contents */
@@ -39,18 +62,26 @@ class CommunicationFieldCalculationService extends ServiceBase {
             try {
                 sql = new Sql( sessionFactory.getCurrentSession().connection() )
 
-                List<GroovyRowResult> resultSet = sql.rows( statement, parameters ) /* TODO: change this to explicitly return one row */
-                def results = []
-                resultSet.each { resultRecord ->
-                    def row = [:]
-                    resultRecord.each { record ->
-                        row.put( record.getKey().toString().toLowerCase(), record.value )
+                List<GroovyRowResult> resultSet = sql.rows( statement, parameters, 0, maxRows )
+                def attributeMap = [:]
+                resultSet.each { row ->
+                    row.each { column ->
+                        String attributeName = column.getKey().toString().toLowerCase()
+                        Object attributeValue = column.value
+                        if (maxRows <= 1) {
+                            attributeMap.put( attributeName, attributeValue )
+                        } else {
+                            // handle array of values per column name
+                            println attributeMap.containsKey( attributeName )
+                            println attributeMap.get( attributeName )
+                            println new ArrayList()
+                            ArrayList values = attributeMap.containsKey( attributeName ) ? attributeMap.get( attributeName ) : new ArrayList()
+                            values.add( attributeValue )
+                            attributeMap.put( attributeName, values )
+                        }
                     }
-                    results.add( formatString( communicationField.formatString, row ) )
                 }
-                return results[0]
-
-
+                return formatString( communicationField.formatString, attributeMap )
             } catch (SQLException e) {
                 throw e
             } finally {
