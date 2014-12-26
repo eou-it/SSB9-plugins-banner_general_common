be617d2aeac08aea405068b15c12697420999155
Ed Delaney <edelaney@ellucian.com>
2014-12-26 16:59:31 -0500

    Add CommunicationLog

---
+++ grails-app/services/net/hedtech/banner/general/communication/log/CommunicationLogService.groovy
@@ -0,0 +1,10 @@
+package net.hedtech.banner.general.communication.log
+/*******************************************************************************
+Copyright 2014 Ellucian Company L.P. and its affiliates.
+*******************************************************************************/
+
+import net.hedtech.banner.service.ServiceBase
+
+class CommunicationLogService extends ServiceBase {
+
+}
