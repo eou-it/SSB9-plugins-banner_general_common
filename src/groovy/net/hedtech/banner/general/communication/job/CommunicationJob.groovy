1aec3a9b026905fcad0199c2f7c3421ade83b83f
Ed Delaney <edelaney@ellucian.com>
2015-01-05 16:34:39 -0500

    remove hibernate config item

---
+++ src/groovy/net/hedtech/banner/general/communication/job/CommunicationJob.groovy
@@ -0,0 +1,20 @@
+/** *****************************************************************************
+ © 2015 SunGard Higher Education.  All Rights Reserved.
+
+ CONFIDENTIAL BUSINESS INFORMATION
+
+ THIS PROGRAM IS PROPRIETARY INFORMATION OF SUNGARD HIGHER EDUCATION
+ AND IS NOT TO BE COPIED, REPRODUCED, LENT, OR DISPOSED OF,
+ NOR USED FOR ANY PURPOSE OTHER THAN THAT WHICH IT IS SPECIFICALLY PROVIDED
+ WITHOUT THE WRITTEN PERMISSION OF THE SAID COMPANY
+ ****************************************************************************** */
+package net.hedtech.banner.general.communication.job
+
+/**
+ * CommunicationJob.
+ *
+ * Date: 1/5/2015
+ * Time: 4:16 PM
+ */
+class CommunicationJob {
+}
