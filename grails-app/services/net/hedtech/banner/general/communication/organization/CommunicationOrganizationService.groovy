8194bf74a1041ca562144a60d2587901a543b471
LakshmiRadhakrishnan <lakshmi.radhakrishnan@ellucian.com>
2015-01-12 08:39:47 -0500

    adding edits to date formats

--- grails-app/services/net/hedtech/banner/general/communication/organization/CommunicationOrganizationService.groovy
+++ grails-app/services/net/hedtech/banner/general/communication/organization/CommunicationOrganizationService.groovy
@@ -24,21 +24,21 @@ class CommunicationOrganizationService extends ServiceBase {
 
         if (commorgn.dateFormat != null) {
             try {
-                def datestring = new SimpleDateFormat(commorgn.dateFormat).format(new Date())
+                def datestring = validateDateFormat(commorgn.dateFormat)
             } catch (Exception e) {
                 throw new ApplicationException(CommunicationOrganization, "@@r1:invalid.date.format@@")
             }
         }
         if (commorgn.dayOfWeekFormat != null) {
             try {
-                def datestring = new SimpleDateFormat(commorgn.dayOfWeekFormat).format(new Date())
+                def datestring = validateDateFormat(commorgn.dayOfWeekFormat)
             } catch (Exception e) {
                 throw new ApplicationException(CommunicationOrganization, "@@r1:invalid.dayofweek.format@@")
             }
         }
         if (commorgn.timeOfDayFormat != null) {
             try {
-                def datestring = new SimpleDateFormat(commorgn.timeOfDayFormat).format(new Date())
+                def datestring = validateDateFormat(commorgn.timeOfDayFormat)
             } catch (Exception e) {
                 throw new ApplicationException(CommunicationOrganization, "@@r1:invalid.timeofday.format@@")
             }
@@ -53,21 +53,21 @@ class CommunicationOrganizationService extends ServiceBase {
 
         if (commorgn.dateFormat != null) {
             try {
-                def datestring = new SimpleDateFormat(commorgn.dateFormat).format(new Date())
+                def datestring = validateDateFormat(commorgn.dateFormat)
             } catch (Exception e) {
                 throw new ApplicationException(CommunicationOrganization, "@@r1:invalid.date.format@@")
             }
         }
         if (commorgn.dayOfWeekFormat != null) {
             try {
-                def datestring = new SimpleDateFormat(commorgn.dayOfWeekFormat).format(new Date())
+                def datestring = validateDateFormat(commorgn.dayOfWeekFormat)
             } catch (Exception e) {
                 throw new ApplicationException(CommunicationOrganization, "@@r1:invalid.dayofweek.format@@")
             }
         }
         if (commorgn.timeOfDayFormat != null) {
             try {
-                def datestring = new SimpleDateFormat(commorgn.timeOfDayFormat).format(new Date())
+                def datestring = validateDateFormat(commorgn.timeOfDayFormat)
             } catch (Exception e) {
                 throw new ApplicationException(CommunicationOrganization, "@@r1:invalid.timeofday.format@@")
             }
@@ -75,4 +75,17 @@ class CommunicationOrganizationService extends ServiceBase {
 
     }
 
+
+    def private validateDateFormat(String dateformat) {
+        /* The simpledateformat is very lenient in creating a new instance.  Doesnt error out for many invalid formats.
+        Here we try to catch most of the very apparent ones by comparing the formatted date with the original format.
+        if they are equal then the original format was not valid and the date was not formatted. 
+         */
+        def datestring = new SimpleDateFormat(dateformat)
+        datestring.setLenient(false)
+        def convertedDate = datestring.format(new Date())
+        if (dateformat.equals(convertedDate.toString()))
+            throw new ApplicationException(CommunicationOrganization, "@@r1:invalid.date.format@@")
+        convertedDate
+    }
 }
