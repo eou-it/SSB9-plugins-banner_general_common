59f4c2132ee612e2ecd843c1a4bd3246937f043f
edelaney <edelaney@ellucian.com>
2015-01-14 21:51:23 -0500

    Add server properties domain object

--- src/groovy/net/hedtech/banner/general/communication/organization/CommunicationEmailServerPropreties.groovy
+++ src/groovy/net/hedtech/banner/general/communication/organization/CommunicationEmailServerPropreties.groovy
@@ -1,12 +1,91 @@
 package net.hedtech.banner.general.communication.organization
 
+import groovy.transform.EqualsAndHashCode
+import groovy.transform.ToString
+
+/*******************************************************************************
+Copyright 2014 Ellucian Company L.P. and its affiliates.
+*******************************************************************************/
+
+import javax.persistence.*
+
 /**
- * Created by mbrzycki on 1/14/15.
+ * Email server protocol properties. Defines the connection information for the email server. entity.
  */
-class CommunicationEmailServerPropreties {
+@Entity
+@ToString
+@EqualsAndHashCode
+@Table(name = "GCBSPRP")
+// @NamedQueries(value = [
+// @NamedQuery(name = "CommunicationEmailServerProperties.fetchByxxxxx",
+//             query = """ FROM CommunicationEmailServerProperties a WHERE xxxxx """)
+// ])
+class CommunicationEmailServerProperties implements Serializable {
+
+    /**
+     * SURROGATE ID: Generated unique numeric identifier for this entity.
+     */
+    @Id
+    @Column(name = "GCBSPRP_SURROGATE_ID")
+    @SequenceGenerator(name = "GCBSPRP_SEQ_GEN", allocationSize = 1, sequenceName = "GCBSPRP_SURROGATE_ID_SEQUENCE")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "GCBSPRP_SEQ_GEN")
     Long id
+
+    /**
+     * SMTP HOST: The host name for the SMTP mail server to send email to
+     */
+    @Column(name = "GCBSPRP_SMTP_HOST")
     String smtpHost
+
+    /**
+     * SMTP PORT: The port number on the host send email to
+     */
+    @Column(name = "GCBSPRP_SMTP_PORT")
     String smtpPort
-//    String transportProtocol // smtp, pop3, imap, etc
-    String securityProtocol // none, ssl, etc.
+
+    /**
+     * SMTP HOST: The SMTP protocal to use. none, ssl, etc.
+     */
+    @Column(name = "GCBSPRP_SECURITY_PROTOCOL")
+    String securityProtocol
+
+    /**
+     * VERSION: Optimistic lock token.
+     */
+    @Version
+    @Column(name = "GCBSPRP_VERSION")
+    Integer version
+
+    /**
+     * ACTIVITY DATE: Date that record was created or last updated.
+     */
+    @Column(name = "GCBSPRP_ACTIVITY_DATE")
+    @Temporal(TemporalType.TIMESTAMP)
+    Date lastModified
+
+    /**
+     * USER ID: The user ID of the person who inserted or last updated this record.
+     */
+    @Column(name = "GCBSPRP_USER_ID")
+    String lastModifiedBy
+
+    /**
+     * DATA ORIGIN: Source system that created or updated the data.
+     */
+    @Column(name = "GCBSPRP_DATA_ORIGIN")
+    String dataOrigin
+
+    static constraints = {
+        lastModified(nullable: true)
+        lastModifiedBy(nullable: true, maxSize: 30)
+        dataOrigin(nullable: true, maxSize: 30)
+        securityProtocol(nullable: true, maxSize: 2000)
+        smtpHost(nullable: true, maxSize: 2000)
+        smtpPort(nullable: true, maxSize: 2000)
+    }
+
+
+    // Read Only fields that should be protected against update
+    public static readonlyProperties = [ 'id' ]
+
 }
