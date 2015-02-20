1010a107937df4fccf9a9d736bd0afb4b4689d12
Vijay Kumar Tiwari <vijay.tiwari@ellucian.com>
2015-02-20 15:08:44 +0530

    Added method to fetch all instructional platforms for a list of codes

--- grails-app/services/net/hedtech/banner/general/overall/ldm/InstructionalPlatformCompositeService.groovy
+++ grails-app/services/net/hedtech/banner/general/overall/ldm/InstructionalPlatformCompositeService.groovy
@@ -28,16 +28,16 @@ class InstructionalPlatformCompositeService {
 
 
     @Transactional(readOnly = true)
-    List<InstructionalPlatform> list( Map params ) {
+    List<InstructionalPlatform> list(Map params) {
         List<InstructionalPlatform> instructionalPlatforms = []
-        RestfulApiValidationUtility.correctMaxAndOffset( params, RestfulApiValidationUtility.MAX_DEFAULT, RestfulApiValidationUtility.MAX_UPPER_LIMIT )
+        RestfulApiValidationUtility.correctMaxAndOffset(params, RestfulApiValidationUtility.MAX_DEFAULT, RestfulApiValidationUtility.MAX_UPPER_LIMIT)
         List allowedSortFields = ['abbreviation', 'title']
-        RestfulApiValidationUtility.validateSortField( params.sort, allowedSortFields )
-        RestfulApiValidationUtility.validateSortOrder( params.order )
-        params.sort = LdmService.fetchBannerDomainPropertyForLdmField( params.sort )
-        List<IntegrationPartnerSystemRule> integrationPartnerSystemRuleList = integrationPartnerSystemRuleService.list( params ) as List
-        integrationPartnerSystemRuleList.each {integrationPartnerSystemRule ->
-            instructionalPlatforms << new InstructionalPlatform( integrationPartnerSystemRule, new Metadata( integrationPartnerSystemRule.dataOrigin), GlobalUniqueIdentifier.findByLdmNameAndDomainId( LDM_NAME, integrationPartnerSystemRule.id )?.guid )
+        RestfulApiValidationUtility.validateSortField(params.sort, allowedSortFields)
+        RestfulApiValidationUtility.validateSortOrder(params.order)
+        params.sort = LdmService.fetchBannerDomainPropertyForLdmField(params.sort)
+        List<IntegrationPartnerSystemRule> integrationPartnerSystemRuleList = integrationPartnerSystemRuleService.list(params) as List
+        integrationPartnerSystemRuleList.each { integrationPartnerSystemRule ->
+            instructionalPlatforms << new InstructionalPlatform(integrationPartnerSystemRule, new Metadata(integrationPartnerSystemRule.dataOrigin), GlobalUniqueIdentifier.findByLdmNameAndDomainId(LDM_NAME, integrationPartnerSystemRule.id)?.guid)
         }
         return instructionalPlatforms
     }
@@ -49,42 +49,58 @@ class InstructionalPlatformCompositeService {
 
 
     @Transactional(readOnly = true)
-    InstructionalPlatform get( String guid ) {
-        GlobalUniqueIdentifier globalUniqueIdentifier = GlobalUniqueIdentifier.fetchByLdmNameAndGuid( LDM_NAME, guid )
+    InstructionalPlatform get(String guid) {
+        GlobalUniqueIdentifier globalUniqueIdentifier = GlobalUniqueIdentifier.fetchByLdmNameAndGuid(LDM_NAME, guid)
 
         if (!globalUniqueIdentifier) {
-            throw new ApplicationException( "instructionalplatform", new NotFoundException() )
+            throw new ApplicationException("instructionalplatform", new NotFoundException())
         }
 
-        IntegrationPartnerSystemRule integrationPartnerSystemRule = IntegrationPartnerSystemRule.get( globalUniqueIdentifier.domainId )
+        IntegrationPartnerSystemRule integrationPartnerSystemRule = IntegrationPartnerSystemRule.get(globalUniqueIdentifier.domainId)
         if (!integrationPartnerSystemRule) {
-            throw new ApplicationException( "instructionalplatform", new NotFoundException() )
+            throw new ApplicationException("instructionalplatform", new NotFoundException())
         }
 
-        return new InstructionalPlatform( integrationPartnerSystemRule, new Metadata( integrationPartnerSystemRule.dataOrigin ), globalUniqueIdentifier.guid );
+        return new InstructionalPlatform(integrationPartnerSystemRule, new Metadata(integrationPartnerSystemRule.dataOrigin), globalUniqueIdentifier.guid);
     }
 
 
-    InstructionalPlatform fetchByIntegrationPartnerSystemId( Long domainId ) {
+    InstructionalPlatform fetchByIntegrationPartnerSystemId(Long domainId) {
         if (null == domainId) {
             return null
         }
-        IntegrationPartnerSystemRule integrationPartnerSystemRule = integrationPartnerSystemRuleService.get( domainId )
+        IntegrationPartnerSystemRule integrationPartnerSystemRule = integrationPartnerSystemRuleService.get(domainId)
         if (!integrationPartnerSystemRule) {
             return null
         }
-        return new InstructionalPlatform( integrationPartnerSystemRule, new Metadata( integrationPartnerSystemRule.dataOrigin ), GlobalUniqueIdentifier.findByLdmNameAndDomainId( LDM_NAME, domainId )?.guid )
+        return new InstructionalPlatform(integrationPartnerSystemRule, new Metadata(integrationPartnerSystemRule.dataOrigin), GlobalUniqueIdentifier.findByLdmNameAndDomainId(LDM_NAME, domainId)?.guid)
     }
 
 
-    InstructionalPlatform fetchByIntegrationPartnerSystemCode( String code ) {
+    InstructionalPlatform fetchByIntegrationPartnerSystemCode(String code) {
         if (!code) {
             return null
         }
-        IntegrationPartnerSystemRule integrationPartnerSystemRule = IntegrationPartnerSystemRule.findByCode( code )
+        IntegrationPartnerSystemRule integrationPartnerSystemRule = IntegrationPartnerSystemRule.findByCode(code)
         if (!integrationPartnerSystemRule) {
             return null
         }
-        return new InstructionalPlatform( integrationPartnerSystemRule, new Metadata( integrationPartnerSystemRule.dataOrigin ), GlobalUniqueIdentifier.findByLdmNameAndDomainId( LDM_NAME, integrationPartnerSystemRule.id )?.guid )
+        return new InstructionalPlatform(integrationPartnerSystemRule, new Metadata(integrationPartnerSystemRule.dataOrigin), GlobalUniqueIdentifier.findByLdmNameAndDomainId(LDM_NAME, integrationPartnerSystemRule.id)?.guid)
+    }
+
+    List<InstructionalPlatform> fetchAllByIntegrationPartnerSystemCode(List<String> codes) {
+        List<InstructionalPlatform> instructionalPlatformList = []
+        List<IntegrationPartnerSystemRule> integrationPartnerSystemRuleList = IntegrationPartnerSystemRule.fetchAllByCode(codes)
+        Map integrationPartnerSystemRuleIdMap = [:]
+        integrationPartnerSystemRuleList.each {
+            integrationPartnerSystemRuleIdMap.put(it.id, it)
+        }
+
+        List<GlobalUniqueIdentifier> integrationPartnerSystemRuleGuids = GlobalUniqueIdentifier.fetchByLdmNameAndDomainSurrogateIds(LDM_NAME, integrationPartnerSystemRuleIdMap.keySet())
+        integrationPartnerSystemRuleGuids.each {
+            IntegrationPartnerSystemRule integrationPartnerSystemRule = integrationPartnerSystemRuleIdMap.get(it.domainId)
+            instructionalPlatformList << new InstructionalPlatform(integrationPartnerSystemRule, new Metadata(integrationPartnerSystemRule.dataOrigin), it.guid)
+        }
+        return instructionalPlatformList
     }
 }
