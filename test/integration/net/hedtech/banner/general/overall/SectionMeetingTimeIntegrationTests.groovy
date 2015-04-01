4cd158812b70e22f4d74d8b981fa320a98fd986e
rahulk <rahul.kumar@ellucian.com>
2015-04-01 12:44:45 +0530

    changed the integration test method as it was failing

--- test/integration/net/hedtech/banner/general/overall/SectionMeetingTimeIntegrationTests.groovy
+++ test/integration/net/hedtech/banner/general/overall/SectionMeetingTimeIntegrationTests.groovy
@@ -1,6 +1,6 @@
 
 /*******************************************************************************
- Copyright 2009-2012 Ellucian Company L.P. and its affiliates.
+ Copyright 2009-2015 Ellucian Company L.P. and its affiliates.
 *******************************************************************************/
 package net.hedtech.banner.general.overall
 import org.junit.Before
