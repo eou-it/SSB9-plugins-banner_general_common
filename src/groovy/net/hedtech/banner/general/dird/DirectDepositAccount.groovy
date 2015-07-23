c74eb5c7bc2b738be30740769057eb930feddac9
Daren_Dunn <daren.dunn@ellucian.com>
2015-07-23 12:02:20 -0400

    add constraints and toString to DirectDepositAccount; correct classname typo

--- src/groovy/net/hedtech/banner/general/dird/DirectDepositAccount.groovy
+++ src/groovy/net/hedtech/banner/general/dird/DirectDepositAccount.groovy
@@ -95,10 +95,57 @@ class DirectDepositAccount implements Serializable {
 	@Column(name = "GXRDIRD_ACHT_CODE")
 	String apAchTransactionTypeCode
 	
+	public String toString() {
+		"""DirectDepositAccount [
+			id= $id,
+			version= $version,
+			dataOrigin= $dataOrigin,
+			pidm= $pidm,
+			status= $status,
+			documentType= $documentType,
+			priority= $priority,
+			apIndicator= $apIndicator,
+			hrIndicator= $hrIndicator,
+			lastModified= $lastModified,
+			lastModifiedBy= $lastModifiedBy,
+			bankAccountNum= $bankAccountNum,
+			bankRoutingNum= $bankRoutingNum,
+			amount= $amount,
+			percent= $percent,
+			accountType= $accountType,
+			addressTypeCode= $addressTypeCode,
+			addressSequenceNum= $addressSequenceNum,
+			intlAchTransactionIndicator= $intlAchTransactionIndicator,
+			isoCode= $isoCode,
+			apAchTransactionTypeCode= $apAchTransactionTypeCode]"""
+	}
+	
+	static constraints = {
+		dataOrigin(nullable: true, maxSize: 30)
+		pidm(nullable: false, min:-99999999, max:99999999)
+		status(nullable: false, maxSize: 1)
+		documentType(nullable: false, maxSize: 1)
+		priority(nullable: false, maxSize: 99)
+		apIndicator(nullable: false, maxSize: 1)
+		hrIndicator(nullable: false, scale: 1)
+		lastModified(nullable: false)
+		lastModifiedBy(nullable: false, maxSize: 30)
+		bankAccountNum(nullable: true, maxSize: 34)
+		bankRoutingNum(nullable: true, maxSize: 11)
+		amount(nullable: true, scale: 2)
+		percent(nullable: true, scale: 2)
+		accountType(nullable: true, maxSize: 1)
+		addressTypeCode(nullable: true, maxSize: 2)
+		addressSequenceNum(nullable: true, maxSize: 99)
+		intlAchTransactionIndicator(nullable: false, maxSize: 8)
+		isoCode(nullable: true, maxSize: 8)
+		apAchTransactionTypeCode(nullable: true, maxSize: 8)
+	}
+	
 	public static fetchByPidm(Map parms) {
 		def dirdAccounts
 
-		DirectDepositAccounts.withSession { session ->
+		DirectDepositAccount.withSession { session ->
 			dirdAccounts = session.getNamedQuery(
 					'LeaveTitle.fetchByPidm')
 					.setString('pidm', parms?.pidm).list()[0]
