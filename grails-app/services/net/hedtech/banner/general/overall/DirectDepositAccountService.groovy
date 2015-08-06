65815cc69eaf96b4cbd960c7147d0f9ff9a4073b
Daren_Dunn <daren.dunn@ellucian.com>
2015-08-06 09:23:17 -0400

    Adding in DirectDepositAccount domain, service, and integration classes fo GXRDIRD table

---
+++ grails-app/services/net/hedtech/banner/general/overall/DirectDepositAccountService.groovy
@@ -0,0 +1,11 @@
+package net.hedtech.banner.general.overall
+
+import net.hedtech.banner.service.ServiceBase
+
+import net.hedtech.banner.general.overall.DirectDepositAccount
+
+class DirectDepositAccountService extends ServiceBase{
+
+    static transactional = true
+
+}
