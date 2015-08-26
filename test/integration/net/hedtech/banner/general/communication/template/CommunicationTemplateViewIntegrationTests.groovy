fa8a9e84b6023fa2a0c3f1b143da71aaf2a89650
Michael Brzycki <michael.brzycki@ellucian.com>
2015-08-26 14:37:34 -0400

    fixed unit test to verify that active on template view is working correctly.

--- test/integration/net/hedtech/banner/general/communication/template/CommunicationTemplateViewIntegrationTests.groovy
+++ test/integration/net/hedtech/banner/general/communication/template/CommunicationTemplateViewIntegrationTests.groovy
@@ -5,10 +5,7 @@ package net.hedtech.banner.general.communication.template
 
 import net.hedtech.banner.general.communication.CommunicationManagementTestingSupport
 import net.hedtech.banner.general.communication.folder.CommunicationFolder
-import net.hedtech.banner.general.communication.population.CommunicationPopulationQuery
-import net.hedtech.banner.general.communication.population.CommunicationPopulationQueryView
 import net.hedtech.banner.testing.BaseIntegrationTestCase
-import org.hibernate.FlushMode
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
@@ -64,86 +61,15 @@ class CommunicationTemplateViewIntegrationTests extends BaseIntegrationTestCase 
         emailTemplate = communicationEmailTemplateService.publish( emailTemplate )
         assertEquals( Boolean.TRUE, emailTemplate.published )
 
+        cleanUpGorm()
         templateView = CommunicationTemplateView.get( emailTemplate.id )
-//        assertEquals( Boolean.TRUE, templateView.active )
+        assertEquals( Boolean.TRUE, templateView.active )
     }
 
-
-//    @Test
-//    void testPopulationQueryViewPagination() {
-//
-//        def populationQuery = newPopulationQuery("MyTestPop")
-//        populationQuery.save(failOnError: true, flush: true)
-//
-//        def queries = CommunicationPopulationQueryView.findByNameWithPagingAndSortParams([params: [name: "MyTestPop"]],
-//                [sortColumn: "name", sortDirection: "asc", max: 5, offset: 0])
-//        assertEquals 1, queries.size()
-//        assertEquals 1, queries.getTotalCount()
-//    }
-//
-//
-//    @Test
-//    void testDeletePopulationQueryView() {
-//        sessionFactory.currentSession.flushMode = FlushMode.AUTO
-//        def populationQuery = newPopulationQuery("MyTestPop")
-//        populationQuery.save(failOnError: true, flush: true)
-//
-//        // Assert domain values
-//        assertNotNull populationQuery?.id
-//        def id = populationQuery.id
-//        def userid = populationQuery.createdBy
-//
-//        // Find the domain
-//        populationQuery = CommunicationPopulationQuery.get(id)
-//        assertNotNull populationQuery
-//
-//        def view = CommunicationPopulationQueryView.fetchById(id)
-//        assertNotNull(view)
-//        assertNotNull(view.name)
-//
-//        //sessionFactory.currentSession.flushMode = FlushMode.AUTO
-//        // Delete the domain
-//        populationQuery.delete(failOnError: true, flush: true)
-//
-//        // Attempt to find the deleted domain
-//        def popQuery = CommunicationPopulationQuery.get(id)
-//        assertNull popQuery
-//
-//        // Attempt to find the deleted domain
-//        def queryView = CommunicationPopulationQueryView.fetchById(id)
-//        assertNull queryView
-//    }
-//
-//
-//    @Test
-//    void testDynamicFinder() {
-//        def populationQuery = newPopulationQuery("TestName1")
-//        populationQuery.save(failOnError: true, flush: true)
-//        populationQuery = newPopulationQuery("TestName2")
-//        populationQuery.save(failOnError: true, flush: true)
-//        // Find the domain
-//        def List<CommunicationPopulationQuery> populationQueries = CommunicationPopulationQuery.findAllByQueryName("TestName")
-//        assertNotNull populationQueries
-//        assertEquals(2, populationQueries.size())
-//    }
-//
-//
-//    private def newPopulationQuery(String popName) {
-//        def populationQuery = new CommunicationPopulationQuery(
-//                // Required fields
-//                folder: testFolder,
-//                createDate: new Date(),
-//                createdBy: "TTTTTTTTTT",
-//                name: popName,
-//                valid: true,
-//
-//                // Nullable fields
-//                description: "TTTTTTTTTT",
-//                sqlString: ""
-//        )
-//
-//        return populationQuery
-//    }
-
+    protected cleanUpGorm() {
+        def session = sessionFactory.currentSession
+        session.flush()
+        session.clear()
+    }
 
 }
