f840e01ed804bb53f646008947bb7bd2f19a0d8c
Michael Brzycki <michael.brzycki@ellucian.com>
2016-05-17 14:29:00 -0400

    Allow bcm users to also create query associations.

--- grails-app/services/net/hedtech/banner/general/communication/population/CommunicationPopulationQueryAssociationService.groovy
+++ grails-app/services/net/hedtech/banner/general/communication/population/CommunicationPopulationQueryAssociationService.groovy
@@ -13,7 +13,7 @@ class CommunicationPopulationQueryAssociationService extends ServiceBase {
     def log = Logger.getLogger(this.getClass())
 
     def preCreate(domainModelOrMap) {
-        if (!CommunicationCommonUtility.userCanAuthorContent()) {
+        if (!CommunicationCommonUtility.userCanCreatePopulation()) {
             throw new ApplicationException(CommunicationPopulationQueryAssociation, "@@r1:operation.not.authorized@@")
         }
     }
