96481c96959d10117cc35d0490f3dbcad9ea0aa5
Manjunath Muniraju <manjunath.muniraju@ellucian.com>
2016-07-11 17:22:09 +0530

    Addresses V6 - test cases

--- test/integration/net/hedtech/banner/general/overall/ldm/AddressCompositeServiceIntegrationTests.groovy
+++ test/integration/net/hedtech/banner/general/overall/ldm/AddressCompositeServiceIntegrationTests.groovy
@@ -4,8 +4,23 @@
 package net.hedtech.banner.general.overall.ldm
 
 import net.hedtech.banner.testing.BaseIntegrationTestCase
+import org.junit.Before
+import org.junit.Test
 
 class AddressCompositeServiceIntegrationTests extends BaseIntegrationTestCase {
 
+    AddressCompositeService addressCompositeService
 
+    @Before
+    public void setUp() {
+        formContext = ['GUAGMNU']
+        super.setUp()
+    }
+
+    @Test
+    void testValidList(){
+        def paginationParams = [:]
+        def addresses = addressCompositeService.list(paginationParams)
+        assertNotNull addresses
+    }
 }
