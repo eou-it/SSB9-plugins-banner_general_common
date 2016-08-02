9eef357859f8a810705d5d87467d68a1f3e8c6a2
Manjunath Muniraju <manjunath.muniraju@ellucian.com>
2016-08-02 17:38:32 +0530

    Making default settings in goriccr for Adresses API unit testing to work as expected

--- test/integration/net/hedtech/banner/general/overall/ldm/AddressCompositeServiceIntegrationTests.groovy
+++ test/integration/net/hedtech/banner/general/overall/ldm/AddressCompositeServiceIntegrationTests.groovy
@@ -3,6 +3,9 @@
  ********************************************************************************* */
 package net.hedtech.banner.general.overall.ldm
 
+import groovy.sql.Sql
+import net.hedtech.banner.general.overall.IntegrationConfiguration
+import net.hedtech.banner.general.overall.IntegrationConfigurationService
 import net.hedtech.banner.testing.BaseIntegrationTestCase
 import org.codehaus.groovy.grails.plugins.testing.GrailsMockHttpServletRequest
 import org.junit.Before
@@ -12,6 +15,10 @@ class AddressCompositeServiceIntegrationTests extends BaseIntegrationTestCase {
 
     AddressCompositeService addressCompositeService
 
+    String PROCESS_CODE = IntegrationConfigurationService.PROCESS_CODE
+    String NATION_ISO = IntegrationConfigurationService.NATION_ISO
+    String COUNTRY_DEFAULT_ISO = IntegrationConfigurationService.COUNTRY_DEFAULT_ISO
+
     @Before
     public void setUp() {
         formContext = ['GUAGMNU']
@@ -20,7 +27,10 @@ class AddressCompositeServiceIntegrationTests extends BaseIntegrationTestCase {
 
     @Test
     void testValidList(){
+        updateGoriccr(PROCESS_CODE, NATION_ISO, 2)
+        updateGoriccr(PROCESS_CODE, COUNTRY_DEFAULT_ISO, "US")
         setAcceptHeader("application/vnd.hedtech.integration.v6+json")
+
         def paginationParams = [:]
         def addresses = addressCompositeService.list(paginationParams)
         assertNotNull addresses
@@ -28,7 +38,10 @@ class AddressCompositeServiceIntegrationTests extends BaseIntegrationTestCase {
 
     @Test
     void testGet(){
+        updateGoriccr(PROCESS_CODE, NATION_ISO, 2)
+        updateGoriccr(PROCESS_CODE, COUNTRY_DEFAULT_ISO, "US")
         setAcceptHeader("application/vnd.hedtech.integration.v6+json")
+
         def paginationParams = [:]
         def addresses = addressCompositeService.list(paginationParams)
         assertNotNull addresses
@@ -48,5 +61,15 @@ class AddressCompositeServiceIntegrationTests extends BaseIntegrationTestCase {
     private void setContentTypeHeader(String mediaType) {
         GrailsMockHttpServletRequest request = LdmService.getHttpServletRequest()
         request.addHeader("Content-Type", mediaType)
+    }
+
+    // use sql to get around the validation process
+    private updateGoriccr(def process, def type, def value) {
+        def sql = new Sql(sessionFactory.getCurrentSession().connection())
+        def sqlUpdate = "update goriccr set goriccr_value = ? where goriccr_sqpr_code = ? and goriccr_icsn_code = ?"
+        sql.executeUpdate(sqlUpdate, [value, process, type])
+        sql.close()
+        def intConf = IntegrationConfiguration.findByProcessCodeAndSettingName(process, type)
+        intConf.refresh()
     }
 }
