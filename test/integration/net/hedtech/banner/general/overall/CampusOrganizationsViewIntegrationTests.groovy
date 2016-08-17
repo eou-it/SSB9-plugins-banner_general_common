be71944881ade2523cc343f057f9eb96da4aa2bf
Manjunath Muniraju <manjunath.muniraju@ellucian.com>
2016-08-17 15:49:34 +0530

    Adding more tests to campus orgs view integration test

--- test/integration/net/hedtech/banner/general/overall/CampusOrganizationsViewIntegrationTests.groovy
+++ test/integration/net/hedtech/banner/general/overall/CampusOrganizationsViewIntegrationTests.groovy
@@ -3,6 +3,8 @@
  ********************************************************************************* */
 package net.hedtech.banner.general.overall
 
+import net.hedtech.banner.general.common.GeneralCommonConstants
+import net.hedtech.banner.general.overall.ldm.GlobalUniqueIdentifier
 import net.hedtech.banner.testing.BaseIntegrationTestCase
 import org.junit.After
 import org.junit.Before
@@ -12,16 +14,26 @@ import org.springframework.dao.InvalidDataAccessResourceUsageException
 
 class CampusOrganizationsViewIntegrationTests extends BaseIntegrationTestCase {
 
+    final String i_success_code_1 = '180'
+    GlobalUniqueIdentifier o_success_guid_1
+
     @Before
     public void setUp() {
         formContext = ['GUAGMNU']
         super.setUp()
+        initializeDataReferences()
     }
 
 
     @After
     public void tearDown() {
         super.tearDown()
+    }
+
+
+    private void initializeDataReferences() {
+        o_success_guid_1 = GlobalUniqueIdentifier.fetchByDomainKeyAndLdmName(i_success_code_1, GeneralCommonConstants.STUDENT_ACTIVITY_LDM_NAME)
+        assertNotNull o_success_guid_1
     }
 
     /**
@@ -61,6 +73,24 @@ class CampusOrganizationsViewIntegrationTests extends BaseIntegrationTestCase {
         shouldFail() {
             existingList.delete(flush: true, onError: true)
         }
+    }
+
+
+    @Test
+    void testFetchByGuid() {
+        CampusOrganizationsView campusOrganizationsView = CampusOrganizationsView.fetchByGuid(o_success_guid_1.guid)
+        assertNotNull campusOrganizationsView
+        assertNotNull campusOrganizationsView.id
+        assertNotNull campusOrganizationsView.campusOrgDesc
+    }
+
+
+    @Test
+    void testFetchByCode() {
+        CampusOrganizationsView campusOrganizationsView = CampusOrganizationsView.fetchByCode(o_success_guid_1.domainKey)
+        assertNotNull campusOrganizationsView
+        assertNotNull campusOrganizationsView.id
+        assertNotNull campusOrganizationsView.campusOrgDesc
     }
 
 }
