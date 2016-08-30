098ceaa5ef192691770f2314d7b0117f35daffb2
Sumeet Yajnik <sumeet.yajnik@ellucian.com>
2016-08-30 10:40:39 +0530

    BAPI-6187:API 9.7 Drop 2: To support multiple API applications in Ellucian Ethos Integration

--- src/groovy/net/hedtech/banner/general/system/ldm/v1/ApiResource.groovy
+++ src/groovy/net/hedtech/banner/general/system/ldm/v1/ApiResource.groovy
@@ -1,5 +1,5 @@
 /*********************************************************************************
- Copyright 2014 Ellucian Company L.P. and its affiliates.
+ Copyright 2014-2016 Ellucian Company L.P. and its affiliates.
  **********************************************************************************/
 package net.hedtech.banner.general.system.ldm.v1
 
@@ -8,11 +8,13 @@ class ApiResource {
     Integer version
     String path
     List<String> events
+    String apiDeployment
 
-    ApiResource ( String resource,Integer version, String path, List<String> events) {
+    ApiResource(String resource, Integer version, String path, List<String> events, String apiDeployment) {
         this.resource = resource
         this.version = version
         this.path = path
         this.events = events
+        this.apiDeployment = apiDeployment
     }
 }
