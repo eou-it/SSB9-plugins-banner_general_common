4f8bd83ce3adbe7763703582d92e7716b4ad02f3
Sumeet Yajnik <sumeet.yajnik@ellucian.com>
2016-09-02 11:37:03 +0530

    BAPI-6314: Update ems-api-resources.xml and ApiResource.groovy files to remove API versions support EMA 2.1.2

--- src/groovy/net/hedtech/banner/general/system/ldm/v1/ApiResource.groovy
+++ src/groovy/net/hedtech/banner/general/system/ldm/v1/ApiResource.groovy
@@ -5,14 +5,12 @@ package net.hedtech.banner.general.system.ldm.v1
 
 class ApiResource {
     String resource
-    Integer version
     String path
     List<String> events
     String apiDeployment
 
-    ApiResource(String resource, Integer version, String path, List<String> events, String apiDeployment) {
+    ApiResource(String resource, String path, List<String> events, String apiDeployment) {
         this.resource = resource
-        this.version = version
         this.path = path
         this.events = events
         this.apiDeployment = apiDeployment
