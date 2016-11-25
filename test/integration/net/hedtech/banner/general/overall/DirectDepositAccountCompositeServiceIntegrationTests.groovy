a20de4ec38426272d1ca6554e781644e9767039e
Navein.Fernandes <Navein.Fernandes@ellucian.com>
2016-11-25 23:55:52 +0530

    HFC-6279:update test case as per platform team directions

--- test/integration/net/hedtech/banner/general/overall/DirectDepositAccountCompositeServiceIntegrationTests.groovy
+++ test/integration/net/hedtech/banner/general/overall/DirectDepositAccountCompositeServiceIntegrationTests.groovy
@@ -292,7 +292,7 @@ class DirectDepositAccountCompositeServiceIntegrationTests extends BaseIntegrati
         LocaleContextHolder.setLocale(new Locale("fr-CA"))
         def symbol = directDepositAccountCompositeService.getCurrencySymbol()
 
-        assertEquals '\u00A0$US', symbol
+        assertEquals '\u00A0$\u00A0US', symbol
     }
 
     @Test
