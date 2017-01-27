f44a9facda5fe5dcc1d934e65f18dabdba1f8de1
Jim Caley <jim.caley@ellucian.com>
2017-01-27 14:19:14 -0500

    Implement BXEGS-407 "Create directory profile page with directory items /current listing/ current display value - BACKEND"

---
+++ test/integration/net/hedtech/banner/general/overall/DirectoryProfileCompositeServiceIntegrationTests.groovy
@@ -0,0 +1,57 @@
+/*******************************************************************************
+ Copyright 2017 Ellucian Company L.P. and its affiliates.
+ *******************************************************************************/
+package net.hedtech.banner.general.overall
+
+import net.hedtech.banner.general.person.PersonUtility
+import net.hedtech.banner.testing.BaseIntegrationTestCase
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+class DirectoryProfileCompositeServiceIntegrationTests extends BaseIntegrationTestCase {
+
+    def directoryProfileCompositeService
+
+    @Before
+    public void setUp() {
+        formContext = ['GUAGMNU']
+        super.setUp()
+
+    }
+
+    @After
+    public void tearDown() {
+        super.tearDown()
+    }
+
+
+    @Test
+    void testFetchDirectoryProfileItemsForUser() {
+        def pidm = PersonUtility.getPerson("GDP000005").pidm
+
+        def result = directoryProfileCompositeService.fetchDirectoryProfileItemsForUser(pidm)
+
+        // TODO: finish this test.  Jim Caley
+        assertNull result
+    }
+
+    @Test
+    void testIsMatchingRoleForDirectoryTypeAndIsMatch() {
+        def pidm = PersonUtility.getPerson("GDP000005").pidm
+
+        def result = directoryProfileCompositeService.isMatchingRoleForDirectoryType(pidm, 'C')
+
+        assertTrue result
+    }
+
+    @Test
+    void testIsMatchingRoleForDirectoryTypeAndIsNotMatch() {
+        def pidm = PersonUtility.getPerson("GDP000005").pidm
+
+        def result = directoryProfileCompositeService.isMatchingRoleForDirectoryType(pidm, 'K')
+
+        assertFalse result
+    }
+
+}
