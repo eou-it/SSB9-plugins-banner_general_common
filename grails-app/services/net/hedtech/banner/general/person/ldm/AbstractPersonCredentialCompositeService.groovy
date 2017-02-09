4c12e758bf49d49604fec040dfa719441cb3271e
Sergio Vera <sergio.vera@ellucian.com>
2017-02-09 17:02:02 -0600

    BAPI-7727: Finished code for PUT

--- grails-app/services/net/hedtech/banner/general/person/ldm/AbstractPersonCredentialCompositeService.groovy
+++ grails-app/services/net/hedtech/banner/general/person/ldm/AbstractPersonCredentialCompositeService.groovy
@@ -4,17 +4,28 @@
 package net.hedtech.banner.general.person.ldm
 
 import net.hedtech.banner.exceptions.ApplicationException
+import net.hedtech.banner.exceptions.BusinessLogicValidationException
 import net.hedtech.banner.exceptions.NotFoundException
 import net.hedtech.banner.general.common.GeneralCommonConstants
 import net.hedtech.banner.general.common.GeneralValidationCommonConstants
+import net.hedtech.banner.general.overall.IntegrationConfigurationService
 import net.hedtech.banner.general.overall.ldm.GlobalUniqueIdentifier
 import net.hedtech.banner.general.overall.ldm.LdmService
 import net.hedtech.banner.general.person.AdditionalID
+import net.hedtech.banner.general.person.PersonAddress
 import net.hedtech.banner.general.person.PersonAdvancedSearchViewService
 import net.hedtech.banner.general.person.PersonBasicPersonBase
+import net.hedtech.banner.general.person.PersonBasicPersonBaseService
+import net.hedtech.banner.general.person.PersonEmail
+import net.hedtech.banner.general.person.PersonIdentificationNameAlternate
 import net.hedtech.banner.general.person.PersonIdentificationNameCurrent
 import net.hedtech.banner.general.person.PersonIdentificationNameCurrentService
+import net.hedtech.banner.general.person.PersonRace
+import net.hedtech.banner.general.person.PersonTelephone
 import net.hedtech.banner.general.person.view.PersonAdvancedSearchView
+import net.hedtech.banner.general.system.ldm.Gender
+import net.hedtech.banner.general.system.ldm.NameTypeCategory
+import net.hedtech.banner.general.utility.DateConvertHelperService
 import net.hedtech.banner.restfulapi.RestfulApiValidationUtility
 import org.springframework.transaction.annotation.Transactional
 
@@ -27,6 +38,10 @@ abstract class AbstractPersonCredentialCompositeService extends LdmService {
 
     PersonCredentialService personCredentialService
 
+    PersonBasicPersonBaseService personBasicPersonBaseService
+
+    IntegrationConfigurationService integrationConfigurationService
+
     abstract protected void prepareDataMapForAll_ListExtension(Collection<Map> entities, Map dataMapForAll)
 
     abstract protected void prepareDataMapForSingle_ListExtension(Map entity,
@@ -34,9 +49,76 @@ abstract class AbstractPersonCredentialCompositeService extends LdmService {
 
     abstract protected def createPersonCredentialDataModel(final Map dataMapForSingle)
 
-    abstract protected def extractDataFromRequestBody(final Map content)
-
     abstract protected def getCredentialTypeToAdditionalIdTypeCodeMap()
+
+    protected Map extractDataFromRequestBody(Map personCredential) {
+        def requestData = [:]
+
+        /* Required in DataModel - Required in Banner */
+        String personGuidInPayload
+        if (personCredential.containsKey("id") && personCredential.get("id") instanceof String) {
+            personGuidInPayload = personCredential?.id?.trim()?.toLowerCase()
+            requestData.put('guid', personGuidInPayload)
+        }
+
+        // UPDATE operation - API SHOULD prefer the resource identifier on the URI, over the payload.
+        String personGuidInURI = personCredential?.id?.trim()?.toLowerCase()
+        if (personGuidInPayload && !personGuidInPayload.equals(personGuidInPayload)) {
+            personCredential.put('id', personGuidInURI)
+            requestData.put('guid', personGuidInURI)
+        }
+
+        if (personCredential.containsKey("credentials") && personCredential.get("credentials") instanceof List) {
+            Collection extractedCreds = extractCredentials(personCredential)
+            requestData.put("credentials", extractedCreds)
+        }
+
+        return requestData
+    }
+
+
+     def extractCredentials(Map content) {
+        if (content.containsKey("credentials") && content.get("credentials") instanceof List) {
+            List credentials = content.get("credentials")
+            credentials.retainAll { it instanceof Map }
+
+            if (credentials.type.contains(CredentialType.SOCIAL_SECURITY_NUMBER.versionToEnumMap[GeneralValidationCommonConstants.VERSION_V8]) && credentials.type.contains(CredentialType.SOCIAL_INSURANCE_NUMBER.versionToEnumMap[GeneralValidationCommonConstants.VERSION_V8])) {
+                throw new ApplicationException(this.class.simpleName, new BusinessLogicValidationException("ssn.sin.both.not.valid", null))
+            }
+
+            Collection extractedCreds = []
+            credentials.each {
+                extractedCreds << extractCredential(it)
+            }
+            return extractedCreds
+        }
+    }
+
+    def extractCredential(Map credentialObj) {
+        CredentialType credentialType
+        String value
+
+        if (credentialObj.containsKey("type") && credentialObj.get("type") instanceof String) {
+            credentialType = CredentialType.getByDataModelValue(credentialObj.get("type").trim(), GeneralValidationCommonConstants.VERSION_V8)
+        }
+
+        if (credentialObj.containsKey("value") && credentialObj.get("value") instanceof String) {
+            // value is required, but no "minLength" constraint.  So it can be zero-length string
+            value = credentialObj.get("value").trim()
+        }
+
+        if (!credentialType || value == null) {
+            throw new ApplicationException(this.class.simpleName, new BusinessLogicValidationException("invalid.credentialType", null))
+        }
+        if (credentialType == CredentialType.SOCIAL_SECURITY_NUMBER && value.length() > 9) {
+            throw new ApplicationException("AbstractPersonCredentialCompositeService", new BusinessLogicValidationException("ssn.length.message", null))
+        }
+        if (credentialType == CredentialType.SOCIAL_INSURANCE_NUMBER && value.length() > 9) {
+            throw new ApplicationException("AbstractPersonCredentialCompositeService", new BusinessLogicValidationException("ssn.length.message", null))
+        }
+
+        return [type: credentialType, value: value]
+    }
 
     private def createPersonCredentialDataModels(def entities) {
         def decorators = []
@@ -125,7 +207,7 @@ abstract class AbstractPersonCredentialCompositeService extends LdmService {
         dataMapForSingle.put("personData", entity)
 
         dataMapForSingle.put("pidm", entity["personIdentificationNameCurrent"].pidm)
-        dataMapForSingle.put("bannerId", entity["personIdentificationNameCurrent"].bannerId)
+        dataMapForSingle.put("bannerId", entity["personIdentificationNameCurrent"]?.bannerId?:null)
         dataMapForSingle.put("guid", entity["globalUniqueIdentifier"].guid)
 
         return dataMapForSingle
@@ -133,7 +215,106 @@ abstract class AbstractPersonCredentialCompositeService extends LdmService {
 
     private def prepareDataMapForSingle_Create(Map entity) {
         Map dataMapForSingle = initDataMapForSingle("CREATE", entity)
+
+        dataMapForSingle.put("credentials", entity.credentials)
         return dataMapForSingle
+    }
+
+
+    @Transactional
+    def update(Map personCredential) {
+
+        def ssnCredentialObj
+        def bannerIdCredentialObj
+        PersonIdentificationNameCurrent personIdentification
+        Map additionalIdTypeCodeToIdMap = [:]
+
+        Map requestData = extractDataFromRequestBody(personCredential)
+
+        String personGuid = requestData.get('guid')
+
+        //retrieve guid
+        GlobalUniqueIdentifier globalUniqueIdentifier = GlobalUniqueIdentifier.fetchByGuid('persons', personGuid)
+        if (!globalUniqueIdentifier) {
+            throw new ApplicationException("Person", new NotFoundException())
+        }
+        requestData.put "globalUniqueIdentifier",globalUniqueIdentifier
+
+        //retrieve person identification
+        Integer pidm = globalUniqueIdentifier.domainKey?.toInteger()
+
+        List<PersonIdentificationNameCurrent> personIdentificationList = PersonIdentificationNameCurrent.findAllByPidmInList([pidm])
+
+        personIdentificationList.each { identification ->
+            if (identification.changeIndicator == null) {
+                personIdentification = identification
+            }
+        }
+        requestData.put "personIdentificationNameCurrent", personIdentification
+
+        //retrieve credentials data
+        if (requestData.containsKey("credentials")) {
+            def personCredentials = requestData.get("credentials")
+
+                ssnCredentialObj = personCredentials.find {
+                    it.type == CredentialType.SOCIAL_SECURITY_NUMBER
+                }
+                if (!ssnCredentialObj) {
+                    ssnCredentialObj = personCredentials.find {
+                        it.type == CredentialType.SOCIAL_INSURANCE_NUMBER
+                    }
+                }
+
+                bannerIdCredentialObj = personCredentials.find {
+                    it.type == CredentialType.BANNER_ID
+                }
+
+                Map credentialTypeToAdditionalIdTypeCodeMap = getCredentialTypeToAdditionalIdTypeCodeMap()
+                credentialTypeToAdditionalIdTypeCodeMap.each { credentialType, additionalIdTypeCode ->
+                    def obj = personCredentials?.find {
+                        it.type == credentialType
+                    }
+                    if (obj) {
+                        log.debug "$credentialType --- $additionalIdTypeCode --- ${obj.value}"
+                        additionalIdTypeCodeToIdMap.put(additionalIdTypeCode, obj.value)
+                        personCredentials.remove(obj)
+                    }
+                }
+        }
+
+        //updates banner id
+        if (bannerIdCredentialObj && (bannerIdCredentialObj.value?.length() > 0 && bannerIdCredentialObj.value?.length() <= 9)) {
+            if (personIdentification.bannerId != bannerIdCredentialObj.value) {
+                PersonIdentificationNameCurrent personIdentificationNameCurrent = PersonIdentificationNameCurrent.fetchByBannerId(bannerIdCredentialObj.value)
+                if (!personIdentificationNameCurrent) {
+                    personIdentification.bannerId = bannerIdCredentialObj.value
+                    personIdentificationNameCurrentService.update(personIdentification)
+                } else {
+                    throw new ApplicationException(this.class.simpleName, new BusinessLogicValidationException("bannerId.already.exists", null))
+                }
+            }
+        }
+
+        //updates alternate ids
+        if (additionalIdTypeCodeToIdMap) {
+            def additionalIdsToRemove = additionalIdTypeCodeToIdMap.findAll { key, value -> !value }
+            additionalIdsToRemove.each { key, value -> additionalIdTypeCodeToIdMap.remove(key) }
+            personCredentialService.deleteAdditionalIDs(personIdentification.pidm, additionalIdsToRemove.keySet())
+            personCredentialService.createOrUpdateAdditionalIDs(personIdentification.pidm, additionalIdTypeCodeToIdMap)
+        }
+
+        //personBasicPersonBase
+        List<PersonBasicPersonBase> personBaseList = PersonBasicPersonBase.findAllByPidmInList([pidm])
+        //updates ssn and sin
+        personBaseList.each { personBase ->
+            if (ssnCredentialObj) {
+                if (personBase.ssn == null || integrationConfigurationService.canUpdatePersonSSN()) {
+                    personBase.ssn = ssnCredentialObj.value
+                    personBasicPersonBaseService.update(personBase)
+                }
+            }
+        }
+        return prepareDataMapForSingle_Create(requestData)
     }
 
     /**
