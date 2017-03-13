194a67aca2e101583f1cfdcd5a88597ecef39360
LakshmiRadhakrishnan <lakshmi.radhakrishnan@ellucian.com>
2017-03-13 18:11:40 -0400

    performance tuning of add profiles

--- src/groovy/net/hedtech/banner/general/communication/population/CommunicationPopulationSelectionListBulkResults.groovy
+++ src/groovy/net/hedtech/banner/general/communication/population/CommunicationPopulationSelectionListBulkResults.groovy
@@ -3,10 +3,17 @@
  *******************************************************************************/
 package net.hedtech.banner.general.communication.population
 
+import net.hedtech.banner.general.communication.CommunicationErrorCode
+
 /**
  * Captures
  */
 class CommunicationPopulationSelectionListBulkResults implements Serializable {
     CommunicationPopulation population
-    List<CommunicationPopulationSelectionListEntryResult> entryResults = new ArrayList<CommunicationPopulationSelectionListEntryResult>()
+    int duplicateCount
+    int ignoredCount
+    int notExistCount
+    int insertedCount
+    String notFoundErrorCode = CommunicationErrorCode.BANNER_ID_NOT_FOUND
+    List<String> bannerIdsNotFound
 }
