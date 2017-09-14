4f3322862af749f4f7da74a5f7b039e02351f66d
LakshmiRadhakrishnan <lakshmi.radhakrishnan@ellucian.com>
2017-09-14 12:58:45 -0400

    updating copyrights

--- grails-app/services/net/hedtech/banner/general/communication/job/CommunicationJobProcessorService.groovy
+++ grails-app/services/net/hedtech/banner/general/communication/job/CommunicationJobProcessorService.groovy
@@ -1,3 +1,6 @@
+/********************************************************************************
+  Copyright 2017 Ellucian Company L.P. and its affiliates.
+********************************************************************************/
 package net.hedtech.banner.general.communication.job
 
 import net.hedtech.banner.general.communication.letter.CommunicationGenerateLetterService
--- grails-app/services/net/hedtech/banner/general/communication/testsend/CommunicationTestSendCompositeService.groovy
+++ grails-app/services/net/hedtech/banner/general/communication/testsend/CommunicationTestSendCompositeService.groovy
@@ -1,3 +1,6 @@
+/********************************************************************************
+  Copyright 2017 Ellucian Company L.P. and its affiliates.
+********************************************************************************/
 package net.hedtech.banner.general.communication.testsend
 
 import net.hedtech.banner.exceptions.ApplicationException
