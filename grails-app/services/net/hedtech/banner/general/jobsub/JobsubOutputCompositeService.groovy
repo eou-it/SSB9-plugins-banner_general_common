8bd2c822b94fcb690afef599d5198e509a07f44b
Margy Hockett <margy.hockett@ellucian.com>
2017-09-29 05:47:45 -0400

    Jobsub Add additional validation to list

--- grails-app/services/net/hedtech/banner/general/jobsub/JobsubOutputCompositeService.groovy
+++ grails-app/services/net/hedtech/banner/general/jobsub/JobsubOutputCompositeService.groovy
@@ -14,39 +14,59 @@ class JobsubOutputCompositeService {
     def jobsubSavedOutputService
     def sessionFactory
 
-    // GET /api/jobsubPendingPrint/<id value>
+    // GET /api/jobsub-pending-print/<id value>
+    /*
+     auth=`echo -n 'grails_user:u_pick_it' | base64`
+     response=`curl -X GET -H "Accept: application/json" -H "Authorization: Basic $auth" -H "Content-Type: APPLICATION/OCTET-STREAM"
+        -H "Content-Disposition: Attachment;Filename="saradms_6256.lis" "http://localhost:8080/BannerGeneralSsb/api/jobsub-pending-print/3"`
+     echo "response:  $response"
+
+   */
     def show(Map params) {
         def job
-        validateParams(params)
         log.debug "Begin show Jobsub Saved output  - Request parameters: ${params}"
-        //if (params.pluralizedResourceName == "jobsub-pending-print") {
+        params = validateParams(params)
+
         job = jobsubSavedOutputService.get(params.id)
         job.jobsubOutput = fetchJobOutputFile(params.id)
 
+        log.debug "End show Jobsub Saved output  - Request parameters: ${params}"
         return job
     }
 
-    // GET /api/jobsubPendingPrint?printer=xxx
+    /***
+     * formats:
+     * get list for single printer:  GET /api/jobsub-pending-print?printer=xxx
+     * get list for list of printers: GET /api/jobsub-pending-print?printer=xxx,yyyy
+     * get all: GET /api/jobsub-pending-print?printer=%
+     *
+     * curl command:
+     * auth=`echo -n 'grails_user:u_pick_it' | base64`
+     * response=`curl -X GET -H "Accept: application/json" -H "Authorization: Basic $auth"  "http://localhost:8080/BannerGeneralSsb/api/jobsub-pending-print?printer=saas1"`
+     * echo "response:  $response"
+     *
+     */
     def list(Map params) {
         log.debug "Begin Pending Print List - Request parameters: ${params}"
         def returnList = [] as List
-        validateParams(params)
+        params = validateParams(params)
 
         returnList = JobsubExternalPrinter.fetchPendingPrintByPrinter(params.printer)
-
+        log.debug "Completed Pending Print List - Request parameters: ${params} Total found: ${returnList?.size()}"
         return returnList
     }
 
 
     Long count(Map params) {
         int total = 0
-        validateParams(params)
+        log.debug "Begin count list Jobsub Saved output  - Request parameters: ${params}"
+        params = validateParams(params)
         total = JobsubExternalPrinter.fetchPendingPrintByPrinter(params.printer)?.size()
         log.debug "Pending print count:End: $total"
         return total
     }
 
-    // put /api/jobsubPendingPrint/<id>
+    // put /api/jobsub-pending-print/<id>
     // @content:  map of update values
     /*
     curl -X PUT -H "Content-Type: application/json" -H "Accept: application/json" -H "Authorization: Basic bWhvY2tldHQ6dV9waWNrX2l0"
@@ -55,8 +75,10 @@ class JobsubOutputCompositeService {
 
 
     def update(def content, def params) {
-        validateParams(params)
+
         log.debug "Jobsub Output Composite Service, Print date update   ${content} params ${params}"
+        params = validateParams(params)
+
         def jobsub = JobsubSavedOutput.get(params.id)
         log.debug "Job ${jobsub?.job} to update Print date"
         if (!jobsub) {
@@ -65,11 +87,12 @@ class JobsubOutputCompositeService {
         jobsub.printDate = new Date()
         log.debug "Job ${jobsub.job} id ${params.id} update print date with ${jobsub.printDate}"
         jobsubSavedOutputService.update([domainModel: jobsub])
-
+        log.debug "End Jobsub Output Composite Service, Print date update ${content} params ${params}"
     }
 
 
     def validateParams(def params) {
+        log.debug "Jobsub Output Composite Service, begin validate params ${params}"
         if (params.containsKey("pluralizedResourceName")) {
             if (params.pluralizedResourceName == "jobsub-pending-print") {
             } else {
@@ -87,25 +110,35 @@ class JobsubOutputCompositeService {
         }
 
         if (params.containsKey("printer")) {
-            if (params.printer == "%") {
-
-            } else if (params.printer =~ ",") {
-                def printers = buildArray(params.printer)
-                params.printer = printers
+            log.debug "Jobsub Output Composite Service, printer params: ${params.printer}"
+            if (params.printer instanceof List) {
+                log.debug "Printer is a list ${params.printer}"
             } else {
-                if (params.printer) {
+                if (params.printer == "%") {
 
                 } else {
-                    throw new ApplicationException(JobsubOutputCompositeService, "@@JobsubOutputCompositeService.missingPrinter@@")
+                    if (params.printer =~ ",") {
+                        def printers = buildArray(params.printer)
+                        params.printer = printers
+                    } else {
+                        if (params.printer) {
+
+                        } else {
+                            throw new ApplicationException(JobsubOutputCompositeService, "@@JobsubOutputCompositeService.missingPrinter@@")
+                        }
+                    }
                 }
             }
+            log.debug "Printer: ${params.printer}"
         }
+        log.debug "Jobsub Output Composite Service, validated params ${params}"
         return params
 
     }
 
+
     private static def buildArray(comp) {
-        comp ? comp.split(",").findAll {!(it.trim() == "")}.collect {it.trim()} : []
+        comp ? comp.split(",").findAll { !(it.trim() == "") }.collect { it.trim() } : []
     }
 
 
