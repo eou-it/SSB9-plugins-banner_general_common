8fed84486bdb1d09ed405d1a37b8f118b997cea7
Bindhu Prakash <bindhu.prakash@ellucian.com>
2018-08-02 15:05:12 -0500

    Added the missing HTML tag in the HTMLHeader for letter creation

--- grails-app/services/net/hedtech/banner/general/communication/letter/pdf/CommunicationLetterPdfBuilder.groovy
+++ grails-app/services/net/hedtech/banner/general/communication/letter/pdf/CommunicationLetterPdfBuilder.groovy
@@ -77,6 +77,7 @@ class CommunicationLetterPdfBuilder {
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append( "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" );
         stringBuilder.append( "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">" );
+        stringBuilder.append( "<html xmlns=\"http://www.w3.org/1999/xhtml\">" );
         stringBuilder.append( "<head><title></title>" );
         stringBuilder.append( getStyleDeclaration() );
         stringBuilder.append( "</head>" );
