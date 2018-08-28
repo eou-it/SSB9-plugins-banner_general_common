f30e45df3cfd3874072158a20d741b287ed67c3e
sivaramn <sivaram.nyapathi@ellucian.com>
2018-08-28 12:32:03 +0530

    NO JIRA : Handling additional exceptions

--- grails-app/services/net/hedtech/banner/general/aip/AipNotificationService.groovy
+++ grails-app/services/net/hedtech/banner/general/aip/AipNotificationService.groovy
@@ -6,6 +6,7 @@ package net.hedtech.banner.general.aip
 import org.apache.log4j.Logger
 import net.hedtech.banner.service.ServiceBase
 import net.hedtech.banner.general.overall.IntegrationConfiguration
+import org.springframework.dao.InvalidDataAccessResourceUsageException
 
 import java.sql.SQLException
 
@@ -30,9 +31,16 @@ public class AipNotificationService extends ServiceBase {
      * @return Boolean
      * */
     public Boolean hasActiveActionItems(Integer pidm) {
-        return UserActiveActionItem.checkIfActionItemPresent(pidm)
+        try {
+            return UserActiveActionItem.checkIfActionItemPresent(pidm)
+        } catch (SQLException e) {
+            LOGGER.warn("Exception while fetching aip enabled flag from goriccr" + e.getMessage())
+            return false
+        } catch (InvalidDataAccessResourceUsageException e) {
+            LOGGER.warn("view GVQ_GCRACPN does not exist or does not have necessary privileges" + e.getMessage())
+            return false
+        }
     }
-
 
     /**
      * checks whether aip is enabled or not
@@ -40,13 +48,15 @@ public class AipNotificationService extends ServiceBase {
      * */
     public String getAipEnabledFlag() {
         def aipEnabledStatus
-        try{
+        try {
             aipEnabledStatus = IntegrationConfiguration.fetchByProcessCodeAndSettingName(SQPR_CODE_GENERAL_SSB, ICSN_CODE_ENABLE_ACTION_ITEMS)?.value == YES ? ENABLED : DISABLED
             LOGGER.debug("AIP Enabled status [GENERAL_SSB,ENABLE.ACTION.ITEMS]=" + aipEnabledStatus)
-        }catch (SQLException e){
+        } catch (SQLException e) {
             aipEnabledStatus = ""
-            LOGGER.warn("Exception while fetching aip enabled flag from goriccr"+e.getMessage())
-
+            LOGGER.warn("Exception while fetching aip enabled flag from goriccr" + e.getMessage())
+        } catch (InvalidDataAccessResourceUsageException e) {
+            aipEnabledStatus = ""
+            LOGGER.warn("Exception while fetching aip enabled flag from goriccr" + e.getMessage())
         }
 
         return aipEnabledStatus
