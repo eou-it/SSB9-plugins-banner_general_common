5c4b904092a420c5abb510ec9cd5c9b1fbb41d37
Supritha Kulkarni <supritha.shrikantkulkarni@ellucian.com>
2019-07-20 02:09:12 +0530

    SSS-193 Incorporated review comments

--- grails-app/services/net/hedtech/banner/general/common/GeneralJsonService.groovy
+++ grails-app/services/net/hedtech/banner/general/common/GeneralJsonService.groovy
@@ -1,14 +1,17 @@
 /*******************************************************************************
  Copyright 2019 Ellucian Company L.P. and its affiliates.
  *******************************************************************************/
-package net.hedtech.banner.general.student
+package net.hedtech.banner.general.common
 
+import net.hedtech.banner.general.common.GeneralJson
 import grails.gorm.transactions.Transactional
+import net.hedtech.banner.service.ServiceBase
 
 @Transactional
-class StudentJsonService {
+class GeneralJsonService extends ServiceBase{
 
     def fetchJsonData(Long id) {
-        return StudentJson.fetchJsonData(id)
+        return GeneralJson.fetchJsonData(id)
     }
+
 }
