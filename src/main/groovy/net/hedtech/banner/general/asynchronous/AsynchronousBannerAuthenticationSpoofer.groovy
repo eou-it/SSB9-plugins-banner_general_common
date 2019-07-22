9e9f7127dde6c44b62cbb4b8a6108f2f4dfc2a06
bindhup <bindhu.prakash@ellucian.com>
2019-07-22 13:35:31 -0500

    Fix to ser form context in the spoofer when the user is COMMMGRHR

--- src/main/groovy/net/hedtech/banner/general/asynchronous/AsynchronousBannerAuthenticationSpoofer.groovy
+++ src/main/groovy/net/hedtech/banner/general/asynchronous/AsynchronousBannerAuthenticationSpoofer.groovy
@@ -29,6 +29,7 @@ import java.sql.SQLException
 public class AsynchronousBannerAuthenticationSpoofer implements AuthenticationProvider {
     private static final log = Logger.getLogger(AsynchronousBannerAuthenticationSpoofer.class)
     public static final monitorOracleUserName = 'COMMMGR'
+    public static final monitorOracleUserNameForHR = 'COMMMGRHR'
 
     def dataSource  // injected by Spring
     MultiEntityProcessingService multiEntityProcessingService
@@ -55,7 +56,7 @@ public class AsynchronousBannerAuthenticationSpoofer implements AuthenticationPr
             if (log.isDebugEnabled()) log.debug("Authenticated as ${monitorOracleUserName} for async task process monitor thread.")
         } else {
             //adding this here for grails 3 as the form context value was getting blanked out at some point
-            if(CommunicationCommonUtility.getUserOracleUserName().equals(monitorOracleUserName)) {
+            if(CommunicationCommonUtility.getUserOracleUserName().equals(monitorOracleUserName) || CommunicationCommonUtility.getUserOracleUserName().equals(monitorOracleUserNameForHR)) {
                 FormContext.set(['CMQUERYEXECUTE'])
             }
             log.debug("Already authenticated as ${SecurityContextHolder.getContext().getAuthentication().principal.toString()}.")
